017eee8369c612534df07dffde5b778092f46a72
DESKTOP-1998JVL\Alexis <neon50360@gmail.com>
2020-11-15 12:08:24 +0100

    initial commit

---
+++ inter/fesl/Playnow.go
@@ -0,0 +1,105 @@
+package fesl
+
+import (
+	"github.com/Synaxis/bfheroesFesl/inter/network"
+	"github.com/Synaxis/bfheroesFesl/inter/mm"
+
+	"github.com/Synaxis/bfheroesFesl/inter/network/codec"
+	"github.com/sirupsen/logrus"
+)
+const (
+	pnow = "pnow"
+)
+
+type reqStart struct {
+	TXN        string `fesl:"TXN"`
+	debugLevel string `fesl:"debugLevel"`
+	Version    int    `fesl:"version"`
+	Partition 	string    `fesl:"partition"`
+
+}
+
+type ansStart struct {
+	TXN    	string        `fesl:"TXN"`
+	ID    	string        `fesl:"id.id"`//exclusive strings from the game
+	Partition 	string    `fesl:"id.partition"`//exclusive strings from the game
+}
+
+// Start handles pnow.Start
+func (fm *Fesl) Start(event network.EvProcess) {
+	logrus.Println("---START---")
+
+	event.Client.Answer(&codec.Packet{
+		Content: ansStart{
+			TXN:  "Start",
+			ID:    "1",
+			Partition: "eagames/bfwest-dedicated",
+		},
+		Send:    event.Process.HEX,
+		Message: pnow,
+	})
+	fm.Status(event)
+
+}
+
+type Status struct {
+	TXN        string                 `fesl:"TXN"`
+	ID         string       		  `fesl:"id.id"`//exclusive strings from the game
+	Partition 	string				  `fesl:"partition"`
+	State      string                 `fesl:"sessionState"`
+	Properties map[string]interface{} `fesl:"props"`
+}
+
+type stGame struct {
+	LobbyID int    `fesl:"lid"`
+	Fit     int    `fesl:"fit"`
+	GID     string `fesl:"gid"` //gameID to join
+}
+
+// Status comes after Start. tells info about desired server
+func (fm *Fesl) Status(event network.EvProcess) {
+	logrus.Println("--Status--")			
+
+
+	var gid string	
+	var err error
+	//search our DB for a gameID (gid) to join fot that indivudal player(uID)
+	err = fm.db.stmtGetBookmark.QueryRow(event.Client.HashState.Get("uID")).Scan(&gid)
+	if err != nil {	
+ 		logrus.Println("no game found for player")
+	}	
+
+
+	// continuos search
+	for search := range mm.Games {
+	gid := search
+	gamesArray := []stGame{
+		{
+			GID:     gid,
+			Fit:     1001,
+			LobbyID: 1,
+		},
+	}		
+
+	//todo if joined = true ( do nothing)
+	//if joined = false ( send canceled STATE)
+
+	event.Client.Answer(&codec.Packet{
+		Content: Status{
+			TXN:    "Status",
+			State:  "COMPLETE",
+			ID:    "1",
+			Partition: "eagames/bfwest-dedicated",
+			Properties: map[string]interface{}{
+				"props.{}": "3", //the n of properties
+				"resultType": "JOIN",
+				"sessionType": "findServer",
+				"games":      gamesArray},
+		},
+		Send:    event.Process.HEX,
+		Message: pnow,
+	})
+	
+	}
+}
+
